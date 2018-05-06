4edfdfcc98866084ee220dbd0b16a17ff4bef568
ThallesMach <35805076+Thalles130@users.noreply.github.com>
2018-05-06 20:34:45 -0300

    ProjetoTestando UpDate

---
+++ ProjetoTestando/src/ProjetoTestando.java
@@ -0,0 +1,14 @@
+
+// Fazendo Pacote com Class do Zero no Eclipse
+
+public class ProjetoTestando {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Tudo ok ");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}

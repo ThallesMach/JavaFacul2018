bedb0dba389b87f85460e065a5250e7ba83ee20c
tls <tls@Machado-PC>
2018-08-19 14:27:01 -0300

    update na Class. Mas ainda não funciona o validador na parte númerica

--- TarefaWalduino2Semestre/src/ViewClienteTeste.java
+++ TarefaWalduino2Semestre/src/ViewClienteTeste.java
@@ -1,7 +1,6 @@
- // https://www.devmedia.com.br/java-swing-conheca-os-componentes-jtextfield-e-jformattedtextfield/30981
 
 
-
+// https://www.devmedia.com.br/java-swing-conheca-os-componentes-jtextfield-e-jformattedtextfield/30981
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout; //gerenciador de layout que especifica como os componentes serão exibidos na Janela.
@@ -19,7 +18,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
 
-public class ViewClienteTeste extends JFrame implements ActionListener{
+
+public class ViewClienteTeste extends JFrame implements ActionListener {
 	private JLabel lblId;
 	private JLabel lblNome;
 	private JLabel lblLogradouro;
@@ -41,110 +41,102 @@ public class ViewClienteTeste extends JFrame implements ActionListener{
 
 	private JPanel painelBotoes;
 	private JPanel painelFormulario;
-	
-	
+
 	public ViewClienteTeste() {
-	   	 super("Clientes");
-         setLayout(new BorderLayout());
-         //
-         //
-         painelFormulario = new JPanel();
-         painelFormulario.setLayout(new GridLayout(7, 2));
-				
+		super("Clientes");
+		setLayout(new BorderLayout());
+		//
+		//
+		painelFormulario = new JPanel();
+		painelFormulario.setLayout(new GridLayout(7, 2));
+
 		lblId = new JLabel("Id: ");
 		painelFormulario.add(lblId);
-		txtId=new JTextField(10);
+		txtId = new JTextField(10);
 		painelFormulario.add(txtId);
-		
+
 		lblNome = new JLabel("Nome: ");
 		painelFormulario.add(lblNome);
-		txtNome=new JTextField(10);
+		txtNome = new JTextField(10);
 		painelFormulario.add(txtNome);
-		
+
 		lblLogradouro = new JLabel("Rua: ");
 		painelFormulario.add(lblLogradouro);
-		txtLogradouro=new JTextField(10);
+		txtLogradouro = new JTextField(10);
 		painelFormulario.add(txtLogradouro);
-		
+
 		lblNumero = new JLabel("N: ");
 		painelFormulario.add(lblNumero);
-		txtNumero=new JTextField(10);
+		txtNumero = new JTextField(10);
 		painelFormulario.add(txtNumero);
-		
+
 		lblComplemento = new JLabel("Complemento: ");
 		painelFormulario.add(lblComplemento);
-		txtComplemento=new JTextField(10);
+		txtComplemento = new JTextField(10);
 		painelFormulario.add(txtComplemento);
-		
+
+
+		// MaskFormatter mascaraId = null;
+		// MaskFormatter mascaraN = null;
+		MaskFormatter mascaraCep = null;
+		MaskFormatter mascaraCpf = null;
+
+		try {
+			// mascaraId = new MaskFormatter("##");
+			// mascaraN = new MaskFormatter("####");
+			mascaraCep = new MaskFormatter("#####-###");
+			mascaraCpf = new MaskFormatter("#########-##");
+			mascaraCep.setPlaceholderCharacter('_');
+			mascaraCpf.setPlaceholderCharacter('_');
+
+		} catch (ParseException excp) {
+			System.err.println("Erro na formatação: " + excp.getMessage());
+			System.exit(-1);
+		}
+
+
 		lblCep = new JLabel("CEP: ");
 		painelFormulario.add(lblCep);
-		txtCep=new JTextField(10);
-		//painelFormulario.add(txtCep);
+		txtCep = new JTextField(10);
+		// painelFormulario.add(txtCep);
+		JFormattedTextField jFormattedTextCep = new JFormattedTextField(mascaraCep);
+		painelFormulario.add(jFormattedTextCep);
 		
 		lblCpf = new JLabel("CPF: ");
 		painelFormulario.add(lblCpf);
-		txtCpf=new JTextField(10);
-		//painelFormulario.add(txtCpf);
+		txtCpf = new JTextField(10);
+		// painelFormulario.add(txtCpf);
+		JFormattedTextField jFormattedTextCpf = new JFormattedTextField(mascaraCpf);
+		painelFormulario.add(jFormattedTextCpf);
 		
-		//MaskFormatter mascaraId = null;
-		//MaskFormatter mascaraN = null;
-		MaskFormatter mascaraCep = null;
-		MaskFormatter mascaraCpf = null;
-		
-		try{
-			//mascaraId = new MaskFormatter("##");
-		    //mascaraN = new MaskFormatter("####");
-            mascaraCep = new MaskFormatter("#####-###");
-            mascaraCpf = new MaskFormatter("#########-##");
-            mascaraCep.setPlaceholderCharacter('_');
-            mascaraCpf.setPlaceholderCharacter('_');
-           
-     }
-     catch(ParseException excp) {
-            System.err.println("Erro na formatação: " + excp.getMessage());
-            System.exit(-1);
-     }
-		
-		
-		//JFormattedTextField jFormattedTextId = new JFormattedTextField(mascaraId);
-		//painelFormulario.add(jFormattedTextId);
-		//JFormattedTextField jFormattedTextN = new JFormattedTextField(mascaraN);
-		//painelFormulario.add(jFormattedTextN);
-		JFormattedTextField jFormattedTextCep = new JFormattedTextField(mascaraCep);
-		painelFormulario.add(jFormattedTextCep);
-        JFormattedTextField jFormattedTextCpf = new JFormattedTextField(mascaraCpf);
-        painelFormulario.add(jFormattedTextCpf);
-        //jFormattedTextCep.setBounds(150,40,100,20);
-        //jFormattedTextCpf.setBounds(150,120,100,20);
-		
-		// Aqui vou criar o painel e colocar os bot?es nele 
+
+
+		// Aqui vou criar o painel e colocar os botões nele
 		painelBotoes = new JPanel();
 		painelBotoes.setLayout(new FlowLayout());
-		
+
 //		painelBotoes = new JPanel();
 //		painelBotoes.add(btnOk);
 //		btnOk.addActionListener(this);
-		
+
 		btnOk = new JButton("Cadastrar");
 		painelBotoes.add(btnOk);
 		btnOk.addActionListener(this);
-		
+
 		btnAlterar = new JButton("Alterar");
 		painelBotoes.add(btnAlterar);
-		
+
 		btnLocalizar = new JButton("Localizar");
 		painelBotoes.add(btnLocalizar);
-		
+
 		btnExcluir = new JButton("Remove");
 		painelBotoes.add(btnExcluir);
-		
 
-		
-		//Agora, vou incluir os dois paineis dentro da janela
+		// Agora, vou incluir os dois paineis dentro da janela
 		add(painelFormulario, BorderLayout.CENTER);
 		add(painelBotoes, BorderLayout.SOUTH);
 	}
-	
+
 //	public void actionPerformed(ActionEvent event) {
 //		if(event.getSource()==btnOk) {
 //			JOptionPane.showMessageDialog(null,"Cliente Cadastrado!");
@@ -152,32 +144,38 @@ public class ViewClienteTeste extends JFrame implements ActionListener{
 //	}
 //	
 //	
-	
-	public void actionPerformed(ActionEvent event ) {
+
+	public void actionPerformed(ActionEvent event) {
 		// if( event.getSource()== btnOk ) {
 		// JOptionPane.showMessageDialog(null, "Cliente Cadastrado!");
-		// 
+		//
 		// }
-		if ( ( txtId.getText().length()>0)
-		                &&(txtNome.getText().length()> 0)
-		                && (txtLogradouro.getText().length() > 0)
-		                && (txtNumero.getText().length() > 0)
-		                && (txtComplemento.getText().length() > 0)
-		                && (txtCep.getText().length() > 0)
-		                && (txtCpf.getText().length() > 0)
-		&& ( event.getSource()== btnOk ) ) {
-		JOptionPane.showMessageDialog(null, "Cliente Cadastrado!");
+		
+		if ((txtId.getText().trim().length() > 0) // trim() == Tirando espaços em branco com metodo 
+													// e length() == Verifica se tem componente preenchido
+				&& (txtNome.getText().trim().length() > 0) 
+				&& (txtLogradouro.getText().trim().length() > 0)
+				&& (txtNumero.getText().trim().length() > 0) 
+				&& (txtComplemento.getText().trim().length() > 0)
+
+//				&& (txtCep.getText().trim().length() > 0 ) 
+//                && (txtCpf.getText().trim().length() > 0)
+				&& ( event.getSource()== btnOk ) ) {
+			JOptionPane.showMessageDialog(null, "Cliente Cadastrado!", "Done", JOptionPane.INFORMATION_MESSAGE);
 		} else {
-		JOptionPane.showMessageDialog(null, "Preencha todos os campos!");
-		            //JOptionPane.showMessageDialog(null, "Confira os campos Obrigat?rios!!");
-		            
-		}}
-	
+			JOptionPane.showMessageDialog(null, "Preencha todos os campos!", "Atenção",JOptionPane.ERROR_MESSAGE);
+//			JOptionPane.showMessageDialog(null, "Confira os campos Obrigatórios!!");
+
+		}		
+	}
+
 	public static void main(String[] args) {
-	   	ViewClienteTeste janelinha = new ViewClienteTeste();//criando a janela
-        janelinha.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        janelinha.setSize(400,250);
-        janelinha.setVisible(true);
+		ViewClienteTeste janelinha = new ViewClienteTeste();// criando a janela
+		janelinha.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janelinha.setSize(400, 250);
+		janelinha.setVisible(true);
+		
+		
 
 	}
 

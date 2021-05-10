127a9db6e6c3af84f95bd098a5b2d1f6e72c64d2
machado <machado@HP-Pavilion-dm4-Notebook-PC>
2021-05-10 18:00:02 -0300

    Teste alterando Linha 17

--- ProjetoTestando/src/ProjetoTestando.java
+++ ProjetoTestando/src/ProjetoTestando.java
@@ -5,15 +5,16 @@ public class ProjetoTestando {
 
 	public static void main(String[] args) {
 		
-		System.out.println(" REVIEW Teste");
+		System.out.println(" REVIEW Teste 02");
 		
-		System.out.println(" Tudo ok ");
+		System.out.println(" Novo Master ");
 		
 		System.out.println(" Testando no Novo -->> Eclipse Photon <<-- ");
 		//Teste recebido no Github
 		System.out.println("----------------------------------------------");
 		System.out.println(" Testando no Novo -->> Version: 2021-03 (4.19.0) <<-- ");
 		// 10/05/2021
+		System.out.println(" -->> Tudo ok ");
 		
 	}
 
